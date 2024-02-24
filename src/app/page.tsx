63d3825d1fb23e025ce10682a59c8d8cc5b09140
Ejiscoexpress <ejiscoexpress@gmail.com>
2024-02-24 11:27:30 +0100

    react-hook-form added.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("prompt")} />
+      <input {...register("prompt", { required: true })} />
       <input {...register("exampleRequired", { required: true })} />
-      {errors.exampleRequired && <span>This field is required</span>}
+      
 
       <input type="submit" />
     </form>
